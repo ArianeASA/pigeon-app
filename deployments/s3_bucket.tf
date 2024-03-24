59a533fe41d8be8f0f953eca5bfb9f61172136a0
ArianeAlves <ArianeAlves@gitlab.com>
2024-03-24 04:20:50 -0300

    create object filter

--- deployments/s3_bucket.tf
+++ deployments/s3_bucket.tf
@@ -9,10 +9,35 @@ resource "aws_s3_bucket_ownership_controls" "lambda_bucket_controls" {
     }
 }
 
-resource "aws_s3_object" "lambda_objetc_filter" {
+resource "aws_s3_object" "lambda_object_filter" {
     bucket = aws_s3_bucket.pigeon_bucket.id
     key    = "test/"
     acl   = "private"
+}
+
+resource "aws_iam_policy" "s3_get_object_pigeon" {
+    name        = "s3_get_object"
+    description = "Allows access to the S3 object"
+
+    policy = jsonencode({
+        Version = "2012-10-17",
+        Statement = [
+            {
+                Effect = "Allow",
+                Action = [
+                    "s3:GetObject"
+                ],
+                Resource = [
+                    "arn:aws:s3:::${aws_s3_bucket.pigeon_bucket.id}/${aws_s3_object.lambda_object_filter.key}"
+                ]
+            }
+        ]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_object_filter_s3_get_object" {
+    role       = aws_iam_role.lambda_exec.name
+    policy_arn = aws_iam_policy.s3_get_object_pigeon.arn
 }
 
 resource "aws_s3_bucket_acl" "lambda_bucket_acl" {
