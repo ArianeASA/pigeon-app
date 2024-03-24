e6a90fcd1150090c47c3f0cf68d243a8516ca8f9
ArianeAlves <ArianeAlves@gitlab.com>
2024-03-24 04:00:47 -0300

    s3 permissions

--- deployments/bucket_main.tf
+++ deployments/bucket_main.tf
@@ -19,3 +19,28 @@ resource "aws_s3_object" "lambda_main" {
   acl   = "private"
   etag = data.archive_file.lambda_zip.output_base64sha256
 }
+
+resource "aws_iam_policy" "s3_get_object" {
+  name        = "s3_get_object"
+  description = "Allows access to the S3 object"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject"
+        ],
+        Resource = [
+          "arn:aws:s3:::${var.bucket_main_id}/${aws_s3_object.lambda_main.key}"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_exec_s3_get_object" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.s3_get_object.arn
+}
