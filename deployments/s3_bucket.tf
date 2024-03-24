3d3c45a7d707532026c296318df64e1bb1e5aa8e
ArianeAlves <ArianeAlves@gitlab.com>
2024-03-24 03:00:50 -0300

    s3 terraform

--- deployments/s3_bucket.tf
+++ deployments/s3_bucket.tf
@@ -19,7 +19,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
     bucket = aws_s3_bucket.pigeon-bucket.id
 
     lambda_function {
-        lambda_function_arn = aws_lambda_function.example.arn
+        lambda_function_arn = aws_lambda_function.pigeon_lambda.arn
         events              = ["s3:ObjectCreated:*"]
         filter_prefix       = "test/"
     }
